003c525e769825d959a5953035b88ec7e73acbc3
Márcia Castro <marciafcastro02@gmail.com>
2026-06-23 19:14:48 +0100

    Add health check endpoint to server
    Added a health check endpoint to confirm server status.

--- server.js
+++ server.js
@@ -35,3 +35,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Backend alive');
+});
